6552f14ccdfd7ae4eea2b7507ed6f82d596ec04e
JC <ju.murgaf@gmail.com>
2021-01-24 22:42:13 -0300

    Proyecto de ckeditor agregado y mejoras al custom generado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import * as customBuild from '../assets/ckCustomBuild/build/ckEditor';
+import * as customBuild from '../assets/ckeditor5/build/ckEditor';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +13,75 @@ export class AppComponent {
   public htmlData: string = '<p>Hello, world!<p>';
   public Editor = customBuild;
   public config = {
-    toolbar: [
-      'heading', '|',
-      'alignment',
-      'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|',
-      'indent', 'outdent', '|',
-      'imageUpload', 'blockQuote', 'insertTable', 'mediaEmbed',
-      'todoList',
-      'undo', 'redo'
-    ],
-    language: 'es'
+    toolbar: {
+      items: [
+        'heading', '|',
+        'bold', 'italic', 'strikethrough', 'underline', 'link', '|',
+        'alignment', 'outdent', 'indent', '|',
+        'bulletedList', 'numberedList', '-',
+        'imageUpload', 'blockQuote', 'insertTable',
+        'undo', 'redo'
+      ],
+      viewportTopOffset: 30,
+      shouldNotGroupWhenFull: true
+    },
+    language: 'es',
+    image: {
+      styles: [
+        'alignLeft', 'alignCenter', 'alignRight'
+      ],
+      toolbar: [
+        'imageResize:50',
+        'imageResize:original',
+
+        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+        // 'imageStyle:full',
+        // 'imageStyle:side',
+        '|',
+        'imageTextAlternative',
+        '|',
+        'linkImage'
+      ],
+      resizeOptions: [
+        {
+          name: 'imageResize:original',
+          value: null,
+          icon: 'original'
+        },
+        {
+          name: 'imageResize:50',
+          value: '50',
+          icon: 'medium'
+        }
+      ],
+    }
+  }
+
+
+  onReady(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return new UploadAdapter(loader);
+    };
+  }
+
+}
+
+
+export class UploadAdapter {
+  loader: any;
+
+  constructor(loader: any) {
+    this.loader = loader;
+  }
+
+  async upload() {
+    return this.loader.file.then(async (file: File) => {
+      return { default: `https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/04/05224508/nota-totoro-10.jpg` };
+    });
+  }
+
+  abort() {
+    console.log('UploadAdapter abort');
   }
 }
+
